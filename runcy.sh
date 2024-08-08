a5a1c79f99d12cba9ff8c0168af47c9af7cae1a5
liuf_cy <1336007004@qq.com>
2024-08-08 19:19:36 +0800

    532

--- runcy.sh
+++ runcy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ps -ef | grep frpc | awk '{print $2}' | xargs kill -9
-nohup /home/liuf/myfrp/frpc -c /home/liuf/myfrp/frpc509.ini &
+nohup /home/liuf/myfrp/frpc -c /home/liuf/myfrp/cy.ini &
